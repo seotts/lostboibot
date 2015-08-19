a5352093a60611c1dcaad60d8543ca6ed3fd66ae
Hayley <hhinsb@gmail.com>
2015-08-18 21:38:48 -0500

    fixed indentation
    I couldn't run the file so I futzed with the indentation until I could.
    no substantial changes here.

--- generate-tweet-list.py
+++ generate-tweet-list.py
@@ -11,7 +11,7 @@ hasFoundBoy = False
  #(or both, if you put it on both ends). This fixes cases where "heartbeat" or "her" (!!!) were 
  #matching "he", etc
 pronouns = {r'\bhe\b':'she', r'\bhim.':'her', r'\bhis\b':'her', r'\bhe\'.':'she\'', r'\bhimself.':'herself', 
-	    r'[\"\']*He\b':'She', r'[\"\']*Him.':'Her', r'[\"\']*His\b':'Her', r'[\"\']*He\'.':'She\'', r'[\"\']*Himself.':'Herself'}
+        r'[\"\']*He\b':'She', r'[\"\']*Him.':'Her', r'[\"\']*His\b':'Her', r'[\"\']*He\'.':'She\'', r'[\"\']*Himself.':'Herself'}
 
 def is_boy_match(word):
    is_match = re.match(r'[\"\']*boy.', word, re.I)
@@ -21,38 +21,39 @@ with open("one-line-text.txt", 'r') as f:
   for line in f:
     for word in line.split():
       if is_boy_match(word):
-	
-	
-	hasFoundBoy = True
-	
-	sentence = ''
-	
-	#add previous words
-	while not previousWords.empty():
-	  sentence = sentence + " " + previousWords.get() + " "
-	
-	#add "boi"
-	if word[0] in ['\"', '\'']:
-	  sentence = sentence + word[:3] + "i" + word[4:]
-	
-	else:
-	  sentence = sentence + word[:2] + "i" + word[3:]
-	  
-	#add next words
-	
+
+
+        hasFoundBoy = True
+
+        sentence = ''
+
+        #add previous words
+        while not previousWords.empty():
+          #maybe we can check if previousWords.get() ==boy?
+          sentence = sentence + " " + previousWords.get() + " "
+
+        #add "boi"
+        if word[0] in ['\"', '\'']:
+          sentence = sentence + word[:3] + "i" + word[4:]
+
+        else:
+          sentence = sentence + word[:2] + "i" + word[3:]
+
+        #add next words
+
       else:
-	for p in pronouns.keys():
-	  if re.match(p, word):
-	    word = "*" + pronouns[p] + "*"
-	if hasFoundBoy:
-	  if(len(sentence) + len(word) < 140):
-	    sentence = sentence + " " + word
-	  else:
-	     print sentence
-	     hasFoundBoy = False
-	else:
-	  previousWords.put(word)
-	  if previousWords.full():
-	    previousWords.get()
-	#print previousWords
+        for p in pronouns.keys():
+          if re.match(p, word):
+            word = "*" + pronouns[p] + "*"
+        if hasFoundBoy:
+          if(len(sentence) + len(word) < 140):
+            sentence = sentence + " " + word
+          else:
+             print sentence
+             hasFoundBoy = False
+        else:
+          previousWords.put(word)
+          if previousWords.full():
+            previousWords.get()
+        #print previousWords
    
